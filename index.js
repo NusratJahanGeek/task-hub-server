3d7448c65830077d82cb0095ad8b6107d8369817
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-06-02 06:25:26 +0600

    CRUD Operation Added for MongoDB Database

--- index.js
+++ index.js
@@ -24,6 +24,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const taskCollection = client.db('taskHub').collection('tasks');
+
+  
+    app.get('/tasks', async(req, res) => {
+      const tasks = await taskCollection.find().toArray();
+      res.json(tasks);
+  });
+
+  app.post('/tasks', async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      if (result.insertedCount === 1) {
+        res.json(newTask);
+      } else {
+        res.status(500).json({ error: 'Failed to insert task' });
+      }
+    } 
+  );
+
+  app.put('/tasks/:id', async (req, res) => {
+    const taskId = req.params.id;
+    const updatedTask = req.body;
+    const { ObjectId } = require('mongodb'); 
+  
+    try {
+      const result = await taskCollection.updateOne({ _id: new ObjectId(taskId) }, { $set: { status: updatedTask.status } });
+      res.json(result.modifiedCount > 0);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to update task' });
+    }
+  });
+  
+
+  app.delete('/tasks/:id', async (req, res) => {
+    const taskId = req.params.id;
+    const result = await taskCollection.deleteOne({ _id: taskId });
+    res.json(result.deletedCount > 0);
+  });
+  
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
